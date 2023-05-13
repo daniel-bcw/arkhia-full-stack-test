965295c8fe8768b4feafda83b5018e675c3f88bd
unknown <muhammadzee112233@gmail.com>
2023-05-13 09:39:41 +0500

    Added CORS for backend resource accessing

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+
+app.use(cors(corsOptions))
 
 app.use(require("./routes/exchangeRoutes"));
 
